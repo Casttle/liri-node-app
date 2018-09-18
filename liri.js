92c9d763c76721bb3af83fa09d7f839e3202a3a8
Casttle <cberry1222@gmail.com>
2018-09-18 11:23:31 -0500

    fixed else statemnts and added switch case

--- liri.js
+++ liri.js
@@ -1,47 +1,63 @@
 require("dotenv").config();
-// else statements are running even whe nthe main command is not used
-// divider is not logging properly
+
 var command = process.argv[2];
 var argCheck = process.argv[3];
 var nodeArgs = process.argv.slice(3).join(" ");
 const request = require("request");
 const keys = require("./keys.js");
 const moment = require("moment");
-const divider = console.log("\n-------------------------------------------\n\n");
 
-function liri (){
+switch (command) {
+    case "concert-this":
+       concert();
+        break;
+    case "spotify-this-song":
+        spot();
+        break;
+    case "movie-this":
+        movie();
+        break;   
+    case "do-what-it-says":
+        random();
+      
+}
+
 //-----Command lines for searching the bands in town API------------------------------------
-if (command === "concert-this") {
+function concert (){
     
     var queryUrl = "https://rest.bandsintown.com/artists/" + nodeArgs + "/events?app_id=codingbootcamp";
     request(queryUrl, function(error, response, body) {
       if (!error && response.statusCode === 200) {
-         const artistInfo = JSON.parse(body)[0]
-        divider
+        
+        const artistInfo = JSON.parse(body)[0]
+
+        console.log("\n-------------------------------------------\n");
         console.log("Venue: " + artistInfo.venue.name +
         "\nVenue Location: " + artistInfo.venue.city + ", " + artistInfo.venue.country +
         "\nDate of event: "+ moment(artistInfo.datetime).format("MMM DD YYYY"));
-        divider
+        console.log("\n-------------------------------------------\n");
       }
     });
-}
+};
 //-----End Bands in town Commands-----------------------------------------------------------
 
 //-----Command lines for searching songs with Spotify--------------------------------------
-if (command === "spotify-this-song" ) {
+function spot(){
+if ( argCheck !== undefined ) {
 
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
- 
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify); 
+
     spotify.search({ type: 'track', query: nodeArgs, limit: 1 }).then(function(response) {
-        // console.log(response.tracks);
+        
         var trackInfo = response.tracks.items[0];
-        divider
+        
+        console.log("\n-------------------------------------------\n");
         console.log("Artist Name: " + trackInfo.album.artists[0].name +
                     "\nSong Name: " + trackInfo.name +
                     "\nPreview link: " + trackInfo.preview_url +        
                     "\nAlbum: " + trackInfo.album.name);
-        divider
+        console.log("\n-------------------------------------------\n");
         }).catch(function(err) {
             console.log(err);
         });
@@ -53,50 +69,55 @@ var spotify = new Spotify(keys.spotify);
         spotify.search({ type: 'track', query: "The Sign", limit: 1 }).then(function(response) {
             // console.log(response.tracks);
             var trackInfo = response.tracks.items[0];
-            divider
+            console.log("\n-------------------------------------------\n");
+            console.log("----Oops no song name was entered. Here is a good one though----\n");
             console.log("Artist Name: " + trackInfo.album.artists[0].name +
                         "\nSong Name: " + trackInfo.name +
                         "\nPreview link: " + trackInfo.preview_url +        
                         "\nAlbum: " + trackInfo.album.name);
-            divider
+            console.log("\n-------------------------------------------\n");            
             }).catch(function(err) {
                 console.log(err);
             });
-
- }
+};
+};
 //-----End Spotify Commands---------------------------------------------------------------
 
 
 //-----Command lines for searching Movies-------------------------------------------------
-if (command === "movie-this") {
+function movie() {
+if (argCheck != undefined ) {
    
     var queryUrl = "http://www.omdbapi.com/?t=" + nodeArgs + "&y=&plot=short&apikey=trilogy";
     request(queryUrl, function(error, response, body) {
       if (!error && response.statusCode === 200) {
          const movieInfo = JSON.parse(body)
-        divider
+        console.log("\n-------------------------------------------\n");
         console.log("Title: " + movieInfo.Title + "\nYear: " + movieInfo.Year + "\nIMDB Rating: " + movieInfo.Ratings[0].Value +
         "\nRotten Tomatoes Rating: " + movieInfo.Ratings[1].Value + "\nCountry: " + movieInfo.Country + "\nLanguage: " + movieInfo.Language +
         "\nPlot: " + movieInfo.Plot + "\nCast: " + movieInfo.Actors);
-        divider
-      }
+        console.log("\n-------------------------------------------\n");
+      };
     });
 } else {
     var queryUrl = "http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=trilogy";
     request(queryUrl, function(error, response, body) {
       if (!error && response.statusCode === 200) {
          const movieInfo = JSON.parse(body)
-        divider
+        console.log("\n-------------------------------------------\n");
+        console.log("----Oops no movie name was entered. Here is a good one though----\n");
         console.log("Title: " + movieInfo.Title + "\nYear: " + movieInfo.Year + "\nIMDB Rating: " + movieInfo.Ratings[0].Value +
         "\nRotten Tomatoes Rating: " + movieInfo.Ratings[1].Value + "\nCountry: " + movieInfo.Country + "\nLanguage: " + movieInfo.Language +
         "\nPlot: " + movieInfo.Plot + "\nCast: " + movieInfo.Actors);
-        divider
+        console.log("\n-------------------------------------------\n");
       }
     });
+};
 }
 //-----End Movie Commands---------------------------------------------------------------
 
-if (command === "do-what-it-says") {
+//-----Random.txt Commands--------------------------------------------------------------
+function random(){
     var fs = require("fs");
 
     fs.readFile("random.txt", "utf8", function(error, data) {
@@ -108,8 +129,6 @@ if (command === "do-what-it-says") {
 
   command = dataArr[0];
   nodeArgs = ["","", dataArr[1]];
-liri();
-        });
-    }
+spot();
+        });   
 };
-liri();
